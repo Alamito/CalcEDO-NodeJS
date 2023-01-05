690b6c17cb2dd0251678dedeadc2d9963f3fb526
Alamito <alamirbobroski12@gmail.com>
2023-01-05 15:13:26 -0300

    feat - add open chrome and write equation

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     await page.goto('https://mathdf.com/dif/pt/');
-    await page.screenshot({ path: "./screenshot/example.png" });
+    
 
+    // seleciona o campo de inserir equacao
+    await page.click('#input-expression');
+
+    // escreve a equacao e "pressiona" enter (\n)
+    await page.type('#input-expression', `y'' + 1/xy'=2xy'\n`);
+
+    await page.screenshot({ path: "./screenshot/example.png" });
     await browser.close();
 })();
