d510936ee0134a0ae91c3d600804354ae1a2555d
Alamito <alamirbobroski12@gmail.com>
2023-01-05 16:24:25 -0300

    feat - make screenshot of result

--- main.js
+++ main.js
@@ -4,15 +4,29 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
+    // vai ate o site da calculadora de EDO
     await page.goto('https://mathdf.com/dif/pt/');
     
-
     // seleciona o campo de inserir equacao
     await page.click('#input-expression');
 
     // escreve a equacao e "pressiona" enter (\n)
-    await page.type('#input-expression', `y'' + 1/xy'=2xy'\n`);
+    await page.type('#input-expression', `(x+4*y)*y'=2*x+3*y-5\n`);
 
-    await page.screenshot({ path: "./screenshot/example.png" });
+    // espera calcular e desenhar a equacao
+    await page.waitForSelector('[class = "load-container hide"]');
+
+    // result recebe o seletor da div onde fica o resultado da EDO
+    const result = await page.$('#math-canvas > div > div:nth-child(2) > div');
+    const onlyEquationResult = await page.$('#math-canvas > div > div:nth-child(2) > div > div:nth-child(8)');
+
+    // faz screenshot de result
+    await result.screenshot({ path: "./screenshot/result.png"});
+    
+    // faz screenshot somente do resultado da EDO
+    await onlyEquationResult.screenshot({path: "./screenshot/onlyEquationResult.png"});
+
+    // fecha o chrome
     await browser.close();
+
 })();
