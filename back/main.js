30437224c95968b7a156e5346ab780f4e949e7e7
Alamito <alamirbobroski12@gmail.com>
2023-01-09 15:00:52 -0300

    feat - delete previous screenshot

--- back/main.js
+++ back/main.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 const serverURL = "http://127.0.0.1:8080/api";
 
 console.log("Back End in execution mode");
 
 const getEquation = (serverURL) => {
+    console.log("Pegando equacao na API");
     const promiseCallback = (resolve) => {
         return resolve(axios.get(serverURL));
     }
@@ -13,9 +15,16 @@ const getEquation = (serverURL) => {
     return new Promise(promiseCallback);
 }
 
+var idPreviousScreenshot = '';
+var idNewScreenshot = '';
+
 const doScreenshotResult = async (equation) => {
+    console.log("Inicializando Puppeteer");
+
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
+
+    console.log("Viajando ate o site da calculadora");
 
     // vai ate o site da calculadora de EDO
     await page.goto('https://mathdf.com/dif/pt/');
@@ -32,25 +41,33 @@ const doScreenshotResult = async (equation) => {
     // result recebe o seletor da div onde fica o resultado da EDO
     const result = await page.$('#math-canvas > div > div:nth-child(2) > div');
 
+    console.log("Tirando screenshot");
+
     // faz screenshot de result
-    const idScreenshot = uuidv4();
-    await result.screenshot({ path: `./screenshot/${idScreenshot}.png`});
+    idPreviousScreenshot = idNewScreenshot;
+    idNewScreenshot = uuidv4();
+    await result.screenshot({ path: `./screenshot/${idNewScreenshot}.png`});
     
     // fecha o chrome
     await browser.close();
 
-    return idScreenshot;
+    return {new: idNewScreenshot, previous: idPreviousScreenshot};
 };
 
-const updateStatusAPI = (serverURL, idScreenshot) => {
+const updateStatusAPI = (serverURL, idNewScreenshot) => {
+    console.log("Atualizando API");
     axios.put(`${serverURL}/1`, {
         equation: '',
         status: "!used",
-        idScreenshot: idScreenshot,
+        idScreenshot: idNewScreenshot,
     })
 }
 
-function tarefas(serverURL) {
+const deletePreviousScreenshot = (idPreviousScreenshot, callback) => {
+    fs.unlink(`./screenshot/${idPreviousScreenshot}.png`, callback);
+}
+
+function tasksForScreenshotResult(serverURL) {
     getEquation(serverURL)
         .then(response => {
             let equation = response.data.equations.data[0].equation;
@@ -59,13 +76,23 @@ function tarefas(serverURL) {
         .then(equation => {
             return doScreenshotResult(equation);
         })
-        .then((idScreenshot) => {
-            updateStatusAPI(serverURL, idScreenshot);
+        .then((Screenshots) => {
+            updateStatusAPI(serverURL, Screenshots.new);
             stopCheckApi = false;
-            console.log('chegou aqui?')
+            return Screenshots;
+        })
+        .then((Screenshots) => {
+            console.log(Screenshots.previous);
+            if (Screenshots.previous != '')
+                deletePreviousScreenshot(Screenshots.previous, function(error) {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log('Successfully deleted previous screenshot');
+                    }
+                })
         });
 }
-
 
 let stopCheckApi = false;
 const checkStatusAPI = setInterval(() => {
@@ -77,12 +104,10 @@ const checkStatusAPI = setInterval(() => {
                 if (status === 'in transit') { 
                     stopCheckApi = true;
                     console.log('Verificacao interrompida...');
-                    console.log(serverURL)
-                    tarefas(serverURL);
+                    tasksForScreenshotResult(serverURL);
                 }
         })
     }
     
-}, 1000);
-
+}, 1000); 
 
